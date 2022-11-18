cad5f44a2efa47a75f29204c4d232e79712d10e4
TheaMarya <118499433+TheaMarya@users.noreply.github.com>
2022-11-18 10:33:54 +0700

    Add files via upload

---
+++ 1_A_71210701.py
@@ -0,0 +1,6 @@
+a = input("Masukkan nama Anda: ")
+b = input("Masukkan nama matakuliah: ")
+c = input("Masukkan grup Anda: ")
+
+print('Haloo!'+ 2*a)
+print("Anda tergabung dalam kelas"+ " "+ b + " " + "pada grup" + " " + c)
---
+++ 2_A_71210701.py
@@ -0,0 +1,4 @@
+fahrenheit = float(input("Suhu dalam skala Fahrenheit :"))
+
+celsius = (fahrenheit - 32)/1.8
+print(str(fahrenheit )+ " derajat Fahrenheit dikonversi menjadi {:.2f}".format(fahrenheit) + " derajat Celsius." )
